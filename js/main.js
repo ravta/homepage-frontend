fecdd42933a1958c62117dd930efefbe80a543e7
Duckquant <job.viktorliddell@gmail.com>
2020-05-20 10:39:14 +0300

    Слайдер готов

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // активируем слайдер
+    if (document.querySelector('#main-slider')) {
+        const mainSlider = new Glide('#main-slider', {
+            type: 'carousel',
+            perView: 1,
+            autoplay: 6000,
+            hoverpause: true,
+            animationDuration: 500,
+            animationTimingFunc: 'ease-in-out',
+        })
 
+        mainSlider.mount()
+    }
 })
